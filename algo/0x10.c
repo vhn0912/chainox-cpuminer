6b547c8c0d89960c87079f34fac86af8b2d461f3
chainox-official <chainox@yandex.ru>
2022-03-01 01:20:06 +0100

    fixed algo

--- algo/0x10.c
+++ algo/0x10.c
@@ -45,6 +45,10 @@ void hash0x10(void *output, const void *input)
 	sph_blake512(&ctx_blake, input, 80);
 	sph_blake512_close(&ctx_blake, hash);
 
+	sph_skein512_init(&ctx_skein);
+	sph_skein512(&ctx_skein, hash, 64);
+	sph_skein512_close(&ctx_skein, hash);
+	
 	sph_bmw512_init(&ctx_bmw);
 	sph_bmw512(&ctx_bmw, hash, 64);
 	sph_bmw512_close(&ctx_bmw, hash);
@@ -53,33 +57,29 @@ void hash0x10(void *output, const void *input)
 	sph_groestl512(&ctx_groestl, hash, 64);
 	sph_groestl512_close(&ctx_groestl, hash);
 
-	sph_skein512_init(&ctx_skein);
-	sph_skein512(&ctx_skein, hash, 64);
-	sph_skein512_close(&ctx_skein, hash);
-
 	sph_jh512_init(&ctx_jh);
 	sph_jh512(&ctx_jh, hash, 64);
 	sph_jh512_close(&ctx_jh, hash);
 
-	sph_keccak512_init(&ctx_keccak);
-	sph_keccak512(&ctx_keccak, hash, 64);
-	sph_keccak512_close(&ctx_keccak, hash);
-
 	sph_luffa512_init(&ctx_luffa);
 	sph_luffa512(&ctx_luffa, hash, 64);
 	sph_luffa512_close(&ctx_luffa, hash);
+	
+	sph_keccak512_init(&ctx_keccak);
+	sph_keccak512(&ctx_keccak, hash, 64);
+	sph_keccak512_close(&ctx_keccak, hash);
 
 	sph_cubehash512_init(&ctx_cubehash);
 	sph_cubehash512(&ctx_cubehash, hash, 64);
 	sph_cubehash512_close(&ctx_cubehash, hash);
 
-	sph_shavite512_init(&ctx_shavite);
-	sph_shavite512(&ctx_shavite, hash, 64);
-	sph_shavite512_close(&ctx_shavite, hash);
-
 	sph_simd512_init(&ctx_simd);
 	sph_simd512(&ctx_simd, hash, 64);
 	sph_simd512_close(&ctx_simd, hash);
+	
+	sph_shavite512_init(&ctx_shavite);
+	sph_shavite512(&ctx_shavite, hash, 64);
+	sph_shavite512_close(&ctx_shavite, hash);
 
 	sph_echo512_init(&ctx_echo);
 	sph_echo512(&ctx_echo, hash, 64);
